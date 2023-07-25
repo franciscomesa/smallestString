b64413b8e9583a5156bc8220f8e1abd479eb19fe
Francisco Mesa <franciscomesa@gmail.com>
2023-07-25 19:17:26 +0100

    :test_tube: Add return first occurrence if characters has only one character

--- test/first.test.ts
+++ test/first.test.ts
@@ -6,7 +6,13 @@ function findSmallestSubstring(inputString: string, characters: string): string 
 
 describe('Smallest string function should', () => {
   const firstInputString = 'TDD development rocks'
+
   it('return empty string if characters is empty', () => {
     expect(findSmallestSubstring(firstInputString, '')).toBe('')
   })
+
+  it ('return first occurrence if characters has only one character', () => {
+    expect(findSmallestSubstring(firstInputString, 'a')).toBe('a')
+  })
+
 })
